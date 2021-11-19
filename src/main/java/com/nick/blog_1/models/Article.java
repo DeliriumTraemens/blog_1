8634af53d1882b6d03c99c20750626d44783c9d4
DeliriumTraemens <oskinnick@gmail.com>
2021-11-19 20:08:10 +0300

    Article and User Adding Lombok Added

--- src/main/java/com/nick/blog_1/models/Article.java
+++ src/main/java/com/nick/blog_1/models/Article.java
@@ -1,11 +1,14 @@
 package com.nick.blog_1.models;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Article {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +19,7 @@ public class Article {
 	private String tag;
 	
 	private String text;
+	
 	
 	//Comments
 	
